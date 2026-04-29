af10e9d9e5ae2885cf2e3a2cc84b69bdf151ba25
zakkerooni <zakkerooni0804@gmail.com>
2026-04-29 15:13:50 +0900

    tes

---
+++ src/pages/api/checkout.js
@@ -0,0 +1,25 @@
+import Stripe from 'stripe';
+const stripe = new Stripe(import.meta.env.STRIPE_SECRET_KEY);
+
+export const POST = async ({ request }) => {
+  const data = await request.formData();
+  const priceId = data.get('priceId');
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [{ price: priceId, quantity: 1 }],
+      mode: 'payment',
+      // 自動で現在のドメイン（pages.devなど）を取得して戻り先を設定します
+      success_url: `${new URL(request.url).origin}/success`,
+      cancel_url: `${new URL(request.url).origin}/`,
+    });
+
+    return new Response(null, {
+      status: 303,
+      headers: { Location: session.url }
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message, { status: 500 });
+  }
+};
